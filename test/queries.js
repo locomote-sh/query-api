30bafc5c23b1ef03cc084b882267edfd6df0bf17
Julian Goacher <julian.goacher@innerfunction.com>
2019-04-04 16:47:53 +0100

    Updated unit tests.

--- test/queries.js
+++ test/queries.js
@@ -17,39 +17,26 @@
 const assert = require('assert');
 
 // Test content origin.
-const Origin = {
-    schema: {
-        version: 1,
-        stores: {
-            'files': {
-                options: {
-                    keyPath: 'pk'
-                },
-                indexes: {
-                    'group': {
-                        keyPath: 'group',
-                        options: { unique: false }
-                    }
+const schema = {
+    name: 'test',
+    version: 1,
+    stores: {
+        'files': {
+            options: {
+                keyPath: 'pk'
+            },
+            indexes: {
+                'group': {
+                    keyPath: 'group',
+                    options: { unique: false }
                 }
             }
         }
     }
 };
 
-// Initialize an in-memory indexeddb.
-const global = {};
-global.window = global;
-
-const initIDB = require('indexeddbshim');
-initIDB( global, {
-    checkOrigin:    false,
-    memoryDatabase: ':memory:'
-});
-
-// Initialize query function.
-const IDB = require('@locomote.sh/idb');
-const idb = IDB( global );
-const query = require('../lib/query')( idb );
+// Initialize an out-of-browser query environment.
+const { query, idb } = require('../lib/external')();
 
 describe('Query tests', () => {
 
@@ -65,47 +52,45 @@ describe('Query tests', () => {
         ];
 
         const {
-            fdbOpenObjStore,
+            idbOpenObjStore,
             idbWrite
         } = idb;
 
-        const objStore = await fdbOpenObjStore( Origin );
+        const objStore = await idbOpenObjStore( schema, 'files', 'readwrite');
 
-        await Promise.all( Data.map( data => idbWrite( data, objStore ) ) );
+        await Promise.all( Data.map( data => idbWrite( objStore, data ) ) );
 
     });
 
     // Tests.
-    test('pk=value',                    { pk: 'aaa' },                  ['aaa']);
-    test('pk$prefix=value',             { pk$prefix: 'a' },             ['a','aa','aaa']);
-    test('pk$from=value',               { pk$from: 'aaa' },             ['aaa','bbb','ccc']);
-    test('pk$to=value',                 { pk$to: 'bbb' },               ['a','aa','aaa','bbb']);
+    test('pk=aaa',                 ['aaa']);
+    test('pk$prefix=a',            ['a','aa','aaa']);
+    test('pk$from=aaa',            ['aaa','bbb','ccc']);
+    test('pk$to=bbb',              ['a','aa','aaa','bbb']);
 
-    test('group=value',                 { group: 'aaa' },               ['a','aa','aaa']);
-    test('group$prefix=value',          { group$prefix: 'aa' },         ['a','aa','aaa']);
-    test('group$from=value',            { group$from: 'bb' },           ['bbb','ccc']);
-    test('group$to=value',              { group$to: 'bb' },             ['a','aa','aaa']);
+    test('group=aaa',              ['a','aa','aaa']);
+    test('group$prefix=aa',        ['a','aa','aaa']);
+    test('group$from=bb',          ['bbb','ccc']); 
+    test('group$to=bb',            ['a','aa','aaa']);
 
-    test('value.title=value',           { 'value.title': 'aaa' },       ['aaa']);
-    test('value.title$prefix=value',    { 'value.title$prefix': 'aa' }, ['aa','aaa']);
-    test('value.title$from=value',      { 'value.title$from': 'bb' },   ['bbb','ccc']);
-    test('value.title$to=value',        { 'value.title$to': 'bb' },     ['a','aa','aaa']);
+    test('value.title=aaa',        ['aaa']);
+    test('value.title$prefix=aa',  ['aa','aaa']);
+    test('value.title$from=bb',    ['bbb','ccc']);
+    test('value.title$to=bb',      ['a','aa','aaa']);
 
-    test('pk=v1 and group=v1',          { pk: 'aaa', group: 'aaa' },    ['aaa']);
-    test('pk=v1 and group=v2',          { pk: 'aaa', group: 'bbb' },    []);
-    test('pk$from=v1 and group=v2',     { pk$from: 'a', group: 'bbb' }, ['bbb','ccc']);
-/*
-*/
+    test('pk=aaa&group=aaa',       ['aaa']);
+    test('pk=aaa&group=bbb',       []);
+    test('pk$from=a&group=bbb',    ['bbb','ccc']);
 });
 
-function test( description, params, expected ) {
+function test( params, expected ) {
 
-    describe( description, () => {
+    describe( params, () => {
 
         let results;
 
         before( async () => {
-            results = await query( Origin, params );
+            results = await query( schema, 'files', params );
         });
 
         it(`should have ${expected.length} results`, () => {
